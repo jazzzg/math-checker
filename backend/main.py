15c269cbded572e48d700e186948fda829ec2510
Jazmin <jazzhang4108@gmail.com>
2026-02-23 13:04:17 -0300

    fix CORS para Vercel

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://math-checker-mocha.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
